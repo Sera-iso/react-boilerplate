13cb426b61431789044a9c0648e846a7d84e9228
Sera <74925057+Sera-iso@users.noreply.github.com>
2021-10-31 20:25:18 +0100

    Added tsx file, updated gitignore and removed gitattributes

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import React from "react-dom";
-
-const App = () => {
-    return <h1>Hello, from react boilerplate</h1>
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
